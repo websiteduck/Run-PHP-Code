5f9b7420af6859e8356fe71090b9ed1ec45a23dc
Brian Adams <brian@websiteduck.com>
2013-06-26 12:00:59 -0500

    Set User-Agent to fix gist imports

--- proxy.php
+++ proxy.php
@@ -6,6 +6,7 @@ if (strpos($url, 'http://', 0) !== 0 && strpos($url, 'https://', 0) !== 0) die()
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: Run-PHP-Code"));
 $content = curl_exec($ch);
 curl_close($ch);
 
