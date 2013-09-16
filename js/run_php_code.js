399a02778da5752567341c84686d583b3b630c26
Brian Adams <brian@websiteduck.com>
2013-09-16 13:28:33 -0500

    Start cleaning up JS

--- js/run_php_code.js
+++ js/run_php_code.js
@@ -1,101 +1,171 @@
-var editor;
-var resizing = false;
-var settings = {};
-var hide_menu_timeout = [];
+/**
+ * Run PHP Code
+ * 
+ * This script gives you the ability to quickly test snippets of PHP code locally.
+ *
+ * @copyright  Copyright 2011-2012, Website Duck LLC (http://www.websiteduck.com)
+ * @link       http://github.com/websiteduck/Run-PHP-Code Run PHP Code
+ * @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+var rpc = {
+
+	loadSettings: function() {
+		rpc.settings = {};
+		if (localStorage.settings) rpc.settings = JSON.parse(localStorage.settings);
+		
+		//Default Settings
+		if (rpc.settings.runExternal === undefined) rpc.settings.runExternal = false;
+	  if (rpc.settings.divideX === undefined || rpc.settings.divideX > $(window).width() - 10) rpc.settings.divideX = $(window).width() / 2;
+	  if (rpc.settings.colorize === undefined) rpc.settings.colorize = true;
+	  if (rpc.settings.theme === undefined) rpc.settings.theme = "twilight";
+	  if (rpc.settings.preWrap === undefined) rpc.settings.preWrap = false;
+	  if (rpc.settings.errorReporting === undefined) rpc.settings.errorReporting = 'fatal';			
+	},
+
+	saveSettings: function() {
+		localStorage.settings = JSON.stringify(rpc.settings);
+	},
+
+	menu: {
+
+		runExternal: function(runExternal, saveSettings = true) {
+	    if (runExternal) {
+	      rpc.settings.runExternal = true;
+	      $('#result_div').hide();
+  	    $('#resize_bar').hide();
+    	  $('#run_php_form').prop('target', 'result_external');
+	    } 
+  	  else {
+    	  rpc.settings.runExternal = false;
+	      $('#result_div').show();
+  	    $('#resize_bar').show();
+    	  $('#run_php_form').prop('target', 'result_frame');
+	    }
+  	  $(window).resize();
+			if (saveSettings) rpc.saveSettings();			
+		},
+
+		colorize: function(colorize, saveSettings = true) {
+			rpc.settings.colorize = colorize;
+			if (saveSettings) rpc.saveSettings();
+		},
+
+		theme: function(theme, saveSettings = true) {
+			rpc.settings.theme = theme;
+			rpc.editor.setTheme("ace/theme/" + rpc.settings.theme);
+			if (saveSettings) rpc.saveSettings();			
+		},
+
+		preWrap: function(preWrap, saveSettings = true) {
+			rpc.settings.preWrap = preWrap;
+			if (saveSettings) rpc.saveSettings();
+		},
+
+		errorReporting: function(errorReporting, saveSettings = true) {
+			rpc.settings.errorReporting = errorReporting;
+			if (saveSettings) rpc.saveSettings();
+		}
+
+	},
+
+	resizeWindow: function(width, height) {
+		if (rpc.settings.runExternal) {
+			$('#code_div').width(width);
+		}
+		else {
+			$('#code_div').width(rpc.settings.divideX - 2);
+			$('#resize_bar').css('left', rpc.settings.divideX - 2 + 'px');
+			$('#result_div').width((width - rpc.settings.divideX) - 2);
+		}
+		rpc.editor.resize();
+	},
+
+	resetEditor: function() {
+		rpc.editor.setValue("<?php\n\n");
+    rpc.editor.gotoLine(3);
+    rpc.editor.focus();
+    window.onbeforeunload = null;
+	},
+
+	setEditorContent: function(content) {
+		rpc.editor.setValue(content);
+		rpc.editor.gotoLine(1);
+		rpc.editor.focus();
+  },
+
+	runCode: function() {
+		$('input[name="phprun_action"]').val('run');
+		$('#run_php_form').submit();
+	}
+
+};
 
 window.onbeforeunload = null;
 
 $(function() {
 
-	if (localStorage.settings) settings = JSON.parse(localStorage.settings);
-	
-	//Default Settings
-	if (settings.run_external === undefined) settings.run_external = false;
-	if (settings.divide_x === undefined || settings.divide_x > $(window).width() - 10) settings.divide_x = $(window).width() / 2;
-	if (settings.colorize === undefined) settings.colorize = true;
-	if (settings.theme === undefined) settings.theme = "twilight";
-	if (settings.pre_wrap === undefined) settings.pre_wrap = false;
-	if (settings.error_reporting === undefined) settings.error_reporting = 'fatal';
-	
-	if (settings.run_external === true) $('#mnu_external_window').prop('checked', true);
-	if (settings.colorize === true) $('#mnu_colorize').prop('checked', true);
-	$('input[name="theme"][value="' + settings.theme + '"]').prop('checked', true);
-	if (settings.pre_wrap === true) $('#mnu_pre_wrap').prop('checked', true);
-	$('input[name="error_reporting"][value="' + settings.error_reporting + '"]').prop('checked', true);
-	
-	editor = ace.edit("code_div");
-    editor.setTheme("ace/theme/" + settings.theme);
-    editor.getSession().setMode("ace/mode/php");
-	editor.setShowPrintMargin(false);
-	editor.commands.addCommand({
+	rpc.loadSettings();
+
+	//Create ACE Editor
+	rpc.editor = ace.edit("code_div");
+	rpc.editor.getSession().setMode("ace/mode/php");
+	rpc.editor.setShowPrintMargin(false);
+	rpc.editor.commands.addCommand({
 		name: 'runCode',
 		bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
 		exec: function(editor) {
-			run_code();
+			rpc.runCode();
 		}
 	});
-	
-	editor.on('change', function() {
+
+	//Set up menu elements
+	var $mnuExternalWindow = $('#mnu_external_window');
+	var $mnuColorize = $('#mnu_colorize');
+	var $mnuPreWrap = $('#mnu_pre_wrap');
+
+	if (rpc.settings.runExternal === true) $mnuExternalWindow.prop('checked', true);
+	if (rpc.settings.colorize === true) $mnuColorize.prop('checked', true);
+	if (rpc.settings.preWrap === true) $mnuPreWrap.prop('checked', true);
+	$('input[name="theme"][value="' + rpc.settings.theme + '"]').prop('checked', true);
+	$('input[name="error_reporting"][value="' + rpc.settings.errorReporting + '"]').prop('checked', true);
+
+	var $mnuTheme = $('input[name="theme"]'); 
+	var $mnuThemeSelected = $mnuTheme.filter(':checked');
+	var $mnuErrorReporting = $('input[name="error_reporting"]'); 
+	var $mnuErrorReportingSelected = $mnuErrorReporting.filter(':checked');
+
+	$mnuExternalWindow.change(function() { rpc.menu.runExternal(this.checked); });
+	rpc.menu.runExternal($mnuExternalWindow.get(0).checked, false);
+
+	$mnuColorize.change(function() { rpc.menu.colorize(this.checked); });
+	rpc.menu.colorize($mnuColorize.get(0).checked, false);
+
+	$mnuTheme.change(function() { rpc.menu.theme(this.value); });
+	rpc.menu.theme($mnuThemeSelected.get(0).value, false);
+
+	$mnuPreWrap.change(function() { rpc.menu.preWrap(this.checked); });
+	rpc.menu.preWrap($mnuPreWrap.get(0).checked, false);
+
+	$mnuErrorReporting.change(function() { rpc.menu.errorReporting(this.value); });
+	rpc.menu.errorReporting($mnuErrorReportingSelected.get(0).value, false);
+
+	rpc.editor.on('change', function() {
 		if (window.onbeforeunload == null) {
 			window.onbeforeunload = function() {
 				return "You have made changes in your editor.";
 			}
 		}
 	});
-	
-	function run_code() {
-		$('input[name="phprun_action"]').val('run');
-		$('#run_php_form').submit();
-	}
 		
-	function reset() {
-		editor.setValue("<?php\n\n");
-		editor.gotoLine(3);
-		editor.focus();
-		window.onbeforeunload = null;
-	}
-	
-	function save_settings() {
-		localStorage.settings = JSON.stringify(settings);
-	}
-	
 	$('#btn_reset').click(function() {
 		var answer = confirm("Are you sure you want to clear the editor?");
-		if (answer) { reset(); run_code(); }
+		if (answer) { rpc.resetEditor(); rpc.runCode(); }
 	});
 	
-	$('#btn_run').click(run_code);
-	
-	$('#mnu_external_window').change(function() {
-		if (this.checked) {
-			settings.run_external = true;
-			$('#result_div').hide();
-			$('#resize_bar').hide();
-			$('#run_php_form').prop('target', 'result_external');
-		} 
-		else {
-			settings.run_external = false;
-			$('#result_div').show();
-			$('#resize_bar').show();
-			$('#run_php_form').prop('target', 'result_frame');
-		}
-		$(window).resize();
-		save_settings();
-	}).change();
-	
-	$('#mnu_colorize').change(function() {
-		if (this.checked) settings.colorize = true;
-		else settings.colorize = false;
-		save_settings();
-	});
-	
-	$('input[name="theme"]').change(function() {
-		settings.theme = $(this).val();
-		editor.setTheme("ace/theme/" + settings.theme);
-		save_settings();
-	});
+	$('#btn_run').click(function() { rpc.runCode(); });
 
-	editor.renderer.on('themeLoaded', function() {
+	rpc.editor.renderer.on('themeLoaded', function() {
 		var bgcolor = $('.ace_gutter').css('backgroundColor'); //base a lot of colors off this one
 		var color = $('#code_div').css('color');
 		$('#title_bar')
@@ -114,7 +184,7 @@ $(function() {
 			.css('box-shadow', '0px 1px 0px 0px ' + shadeColor(bgcolor,20) + ' inset')
 			.css('color', color);
 		$('#resize_bar').css('backgroundColor', shadeColor(bgcolor,-20));
-		if (settings.colorize) {
+		if (rpc.settings.colorize) {
 			$('#result_frame').css('backgroundColor', shadeColor(bgcolor,-2));
 			$('#result_frame').contents().find('html')
 				.css('backgroundColor', shadeColor(bgcolor,-2))
@@ -125,7 +195,7 @@ $(function() {
 		});
 	});
 	$('#result_frame').load(function() { 
-		if (settings.colorize) {
+		if (rpc.settings.colorize) {
 			var bgcolor = $('.ace_gutter').css('backgroundColor'); //base a lot of colors off this one
   	  var color = $('#code_div').css('color');
 
@@ -135,52 +205,32 @@ $(function() {
 		}
 	});
 	
-	$('#mnu_pre_wrap').change(function() {
-		if (this.checked) settings.pre_wrap = true;
-		else settings.pre_wrap = false;
-		save_settings();
-	});
+	$(window).resize(function() { rpc.resizeWindow($(this).width(), $(this).height()); }).resize();
 	
-	$('input[name="error_reporting"]').change(function() {
-		settings.error_reporting = $(this).val();
-		save_settings();
-	});
-	
-	$(window).resize(function() {
-		var page_width = $(window).width();		
-		if (settings.run_external) {
-			$('#code_div').width(page_width);
-		}
-		else {
-			$('#code_div').width(settings.divide_x - 2);
-			$('#resize_bar').css('left', settings.divide_x - 2 + 'px');
-			$('#result_div').width((page_width - settings.divide_x) - 2);
-		}
-		editor.resize();
-	}).resize();
-	
+	var resizing = false;
 	$(document).mousemove(function(e) {
 		if (resizing === true) {
 			var x = e.pageX;
 			if (x < 100) x = 100;
-			settings.divide_x = x;
+			rpc.settings.divideX = x;
 			$(window).resize();
 		}
 	});
 	$('#resize_bar').mousedown(function(e) {
-		if (settings.run_external === false) {
+		if (rpc.settings.runExternal === false) {
 			resizing = true; 
 			$('#result_frame').css('pointer-events', 'none');
-			event.preventDefault();
+			e.preventDefault();
 		}
 	}).mouseup(function(e) {
 		if (resizing) {
 			resizing = false;
 			$('#result_frame').css('pointer-events', 'auto');
-			save_settings();
+			rpc.saveSettings();
 		}
 	});
 		
+	var hide_menu_timeout = [];
 	$('.drop').hover(function() {
 		clearTimeout(hide_menu_timeout[$(this).uniqueId().attr('id')]);
 		$('.drop div').css('z-index', '9997');
@@ -209,24 +259,16 @@ $(function() {
 		$(this).css('backgroundColor', shadeColor($(this).css('backgroundColor'), -5));
 	});
 
-	
-	
 	function get_id_from_url(url) {
 		var url_array = url.split('/');
 		return url_array[url_array.length-1];
-	}
-	
-	function set_editor_content(content) {
-		editor.setValue(content);
-		editor.gotoLine(1);
-		editor.focus();
 	}
 	
 	$('#btn_import').click(function() {
 		var code_url = prompt('Always make sure imported code is safe before running!!!\n\nSupported services: gist.GitHub.com, PasteBin.com, Pastie.org\n\nEnter URL:');
 		if (code_url === null || code_url === '') return;
 		code_id = get_id_from_url(code_url);
-		editor.setValue('Loading code...');
+		rpc.editor.setValue('Loading code...');
 		
 		if (code_url.toLowerCase().indexOf('github.com') !== -1) {
 			$.get('proxy.php', {url: 'https://api.github.com/gists/' + code_id}, function(data) {
@@ -234,21 +276,21 @@ $(function() {
 					data = $.parseJSON(data);
 					var content = '';
 					for (var i in data.files) content += data.files[i].content + '\n';
-					set_editor_content(content);
+					rpc.setEditorContent(content);
 				}
 				else {
-					set_editor_content(data);
+					rpc.setEditorContent(data);
 				}
 			}, 'text');
 		}
 		else if (code_url.toLowerCase().indexOf('pastebin.com') !== -1) {
 			$.get('proxy.php', {url: 'http://pastebin.com/raw.php?i=' + code_id}, function(data) {
-				set_editor_content(data);
+				rpc.setEditorContent(data);
 			}, 'text');
 		}
 		else if (code_url.toLowerCase().indexOf('pastie.org') !== -1) {
 			$.get('proxy.php', {url: 'http://pastie.org/pastes/' + code_id + '/download'}, function(data) {
-				set_editor_content(data);
+				rpc.setEditorContent(data);
 			}, 'text');
 		}
 	});
@@ -261,14 +303,14 @@ $(function() {
 		$('#run_php_form').submit();
 	});
 	
-	$('#title_bar').click(function() { editor.focus(); });
+	$('#title_bar').click(function() { rpc.editor.focus(); });
 	
-	reset();
-	if (settings.run_external === false) run_code();
+	rpc.resetEditor();
+	if (rpc.settings.runExternal === false) rpc.runCode(); //If running externally we don't want a new tab to open right away
 });
 
 function run_php_form_submit() {
-	$('#phprun_code').val(editor.getValue());
+	$('#phprun_code').val(rpc.editor.getValue());
 }	
 
 //http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color
@@ -287,15 +329,13 @@ $.cssHooks.backgroundColor = {
         if (elem.currentStyle)
             var bg = elem.currentStyle["backgroundColor"];
         else if (window.getComputedStyle)
-            var bg = document.defaultView.getComputedStyle(elem,
-                null).getPropertyValue("background-color");
+            var bg = document.defaultView.getComputedStyle(elem, null).getPropertyValue("background-color");
+
         if (bg.search("rgb") == -1)
             return bg;
         else {
             bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-            function hex(x) {
-                return ("0" + parseInt(x).toString(16)).slice(-2);
-            }
+            function hex(x) { return ("0" + parseInt(x).toString(16)).slice(-2); }
             return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
         }
     }
